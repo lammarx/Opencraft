d0eede8aa1cd5a6a689f3b4cd270778647a4d078
lammarx <perlamutrovy080@gmail.com>
2023-10-29 01:09:36 +0300

    Add project files.

---
+++ Minecraft/Minecraft.cpp
@@ -0,0 +1,44 @@
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#define GLEW_STATIC
+
+#define WINDOW_X 1280
+#define WINDOW_Y 720
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_X, WINDOW_Y, "Minecraft v0 | Debug", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW Library" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, WINDOW_X, WINDOW_Y);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
